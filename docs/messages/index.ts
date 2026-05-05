b35dcb8f78b1dd8d8591cd54e0a06fbeda998074
Benjamin Godfrey <benjamin.godfrey.dev@gmail.com>
2026-05-05 21:24:23 +0100

    feat: create message templates

---
+++ docs/messages/index.ts
@@ -0,0 +1,13 @@
+import { draftSTARAnswersMessage } from "./draftSTARAnswers";
+import { findMisunderstoodPhrasesMessage } from "./findMisunderstoodPhrases";
+import { identifyRelevantSupertasksMessage } from "./identifyRelevantSupertasks";
+import { indentifySkillsInTaskMessage } from "./identifySkillsInTask";
+import { promptUserToRethinkTargetSkillsMessage } from "./promptUserToRethinkTargetSkills";
+
+export {
+    draftSTARAnswersMessage,
+    findMisunderstoodPhrasesMessage,
+    identifyRelevantSupertasksMessage,
+    indentifySkillsInTaskMessage,
+    promptUserToRethinkTargetSkillsMessage
+}
